e58f13be2673b77f10288bba08eef860926d63a7
jgero <33359660+jgero@users.noreply.github.com>
2021-04-20 23:50:53 +0200

    task setup and service for harddrive backups

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ ln -sf $PWD/nvim $HOME/.config/nvim
 # git
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 
+# taskwarrior config
+ln -sf $PWD/.taskrc $HOME/.taskrc
+
 # bash stuff - see readme what it takes to make this work
 ln -sf $PWD/bash $HOME/.bashrc.d
 
@@ -15,6 +18,7 @@ ln -sf $PWD/bash $HOME/.bashrc.d
 cp $PWD/systemd/restic/settings.env $HOME/.config/systemd/user/restic-settings.env
 # backups with restic
 cp $PWD/systemd/restic/backup.service $HOME/.config/systemd/user/restic-backup.service
+cp $PWD/systemd/restic/backupToHarddrive.service $HOME/.config/systemd/user/backupToHarddrive.service
 cp $PWD/systemd/restic/backup.timer $HOME/.config/systemd/user/restic-backup.timer
 # prune restic repo
 cp $PWD/systemd/restic/prune.service $HOME/.config/systemd/user/restic-prune.service
