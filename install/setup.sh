a631b9480afb8bf034efe435aa9ce81efde6bd88
jgero <33359660+jgero@users.noreply.github.com>
2021-04-28 00:00:43 +0200

    systemd unit setup started

--- install/setup.sh
+++ install/setup.sh
@@ -16,52 +16,40 @@ if [[ $MOVE_CONFIG ]]; then
 fi
 
 # link the config dir
-ln -sv ~/repos/dotfiles/xdg_config ~/.config
+ln -sv $HOME/repos/dotfiles/xdg_config $HOME/.config
 
 # copy old config back in and delete temp directory
 if [[ $MOVE_CONFIG ]]; then
-    cp -r ~/_temp_config/* ~/.config/
-    rm -rf ~/_temp_config
+    cp -r $HOME/_temp_config/* $HOME/.config/
+    rm -rf $HOME/_temp_config
 fi
 
 ############################# home_config ####################################
 
 # link the bashrc
-if ! [[ -f "~/.bashrc" ]]; then
-    ln -vs ~/repos/dotfiles/home/.bashrc ~/.bashrc
+if ! [[ -f "$HOME/.bashrc" ]]; then
+    ln -vs $HOME/repos/dotfiles/home/.bashrc $HOME/.bashrc
 fi
 
 # link bash user function dir
 if [[ -d "$HOME/.bashrc.d" ]]; then
     MOVE_CONFIG=true
     mv "$HOME/.bashrc.d" "$HOME/_temp_bashrc"
-    ln -sv ~/repos/dotfiles/home/bashrc.d ~/.bashrc.d
-    cp -r ~/_temp_bashrc/* ~/.bashrc.d/
-    rm -rf ~/_temp_bashrc
+    ln -sv $HOME/repos/dotfiles/home/bashrc.d $HOME/.bashrc.d
+    cp -r $HOME/_temp_bashrc/* $HOME/.bashrc.d/
+    rm -rf $HOME/_temp_bashrc
 else
-    ln -sv ~/repos/dotfiles/home/bashrc.d ~/.bashrc.d
+    ln -sv $HOME/repos/dotfiles/home/bashrc.d $HOME/.bashrc.d
 fi
 
 # link bash user autocompletion dir
 if [[ -d "$HOME/.bash_completion.d" ]]; then
     MOVE_CONFIG=true
     mv "$HOME/.bash_completion" "$HOME/_temp_bash_completion"
-    ln -sv ~/repos/dotfiles/home/bash_completion.d ~/.bash_completion.d
-    cp -r ~/_temp_bash_completion/* ~/.bash_completion.d/
-    rm -rf ~/_temp_bash_completion
+    ln -sv $HOME/repos/dotfiles/home/bash_completion.d $HOME/.bash_completion.d
+    cp -r $HOME/_temp_bash_completion/* $HOME/.bash_completion.d/
+    rm -rf $HOME/_temp_bash_completion
 else
-    ln -sv ~/repos/dotfiles/home/bash_completion.d ~/.bash_completion.d
+    ln -sv $HOME/repos/dotfiles/home/bash_completion.d $HOME/.bash_completion.d
 fi
-
-# # systemd services with config files
-
-# # restic settings
-# cp $PWD/systemd/restic/settings.env $HOME/.config/systemd/user/restic-settings.env
-# # backups with restic
-# cp $PWD/systemd/restic/backup.service $HOME/.config/systemd/user/restic-backup.service
-# cp $PWD/systemd/restic/backupToHarddrive.service $HOME/.config/systemd/user/backupToHarddrive.service
-# cp $PWD/systemd/restic/backup.timer $HOME/.config/systemd/user/restic-backup.timer
-# # prune restic repo
-# cp $PWD/systemd/restic/prune.service $HOME/.config/systemd/user/restic-prune.service
-# cp $PWD/systemd/restic/prune.timer $HOME/.config/systemd/user/restic-prune.timer
 
