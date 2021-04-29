caafc7b30bf926c84225ea285df63e2ed48c0a75
jgero <33359660+jgero@users.noreply.github.com>
2021-04-29 21:57:03 +0200

    nice lock screen

--- install/packages.sh
+++ install/packages.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 sudo dnf install \
-    git make g++ unzip cmake curl ripgrep fzf alacritty nodejs npm \
-    xrandr arandr tlp \
+    git make g++ unzip cmake curl ripgrep fzf alacritty nodejs npm ImageMagick \
+    xrandr arandr redshift tlp \
     task restic thunderbird
