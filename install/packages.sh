79eba64c81b9873475a45810b61910589f6ed8c7
jgero <33359660+jgero@users.noreply.github.com>
2021-04-30 18:57:02 +0200

    xclip yank to clipboard support

--- install/packages.sh
+++ install/packages.sh
@@ -2,6 +2,6 @@
 
 sudo dnf install \
     git make g++ unzip cmake curl ripgrep fzf alacritty nodejs npm ImageMagick \
-    i3 \
+    i3 xclip \
     xrandr arandr redshift tlp xinput \
     task restic thunderbird
