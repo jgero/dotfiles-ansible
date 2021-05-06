fbea9a3a725e610d1db2befc478afa1e0712a431
jgero <33359660+jgero@users.noreply.github.com>
2021-05-06 23:22:49 +0200

    added select project keybinding to switch tmux sessions

--- home/bashrc.d/keybindings.bash
+++ home/bashrc.d/keybindings.bash
@@ -3,3 +3,7 @@ if [ -x "$(command -v fzf)"  ]
 then
   source /usr/share/fzf/shell/key-bindings.bash
 fi
+
+# bind ctrl-f to the tmux session switcher
+bind -x '"\C-f": "$HOME/repos/dotfiles/scripts/selectProject"'
+
