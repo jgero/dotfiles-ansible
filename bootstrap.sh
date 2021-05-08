611ef26afeb161797b0cb39b07605c57fcdb1113
jgero <33359660+jgero@users.noreply.github.com>
2021-05-08 18:56:28 +0200

    only run dnf install in bootstrap if necessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,11 @@ set -e
 # dotfiles' project root directory
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# install ansible
-sudo dnf install ansible
+# install ansible if it's missing
+dnf list installed "ansible" > /dev/null || sudo dnf install ansible
 
 # run Ansible playbook using our user
-ansible-playbook -i "$DOTFILES/hosts" "$DOTFILES/dotfiles.yml" --ask-become-pass
+ansible-playbook -i "$DOTFILES/hosts" "$DOTFILES/dotfiles.yml"
 
 exit 0
 
