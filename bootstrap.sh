25b4d3ffa7735ff59a40f23a08a7af29a3e58857
jgero <33359660+jgero@users.noreply.github.com>
2021-05-09 18:22:58 +0200

    gnome keybinding and pip dependency fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # install ansible if it's missing
 dnf list installed "ansible" > /dev/null || sudo dnf install ansible
 # install community collection for dconf plugin
-ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general && pip3 install psutil
 
 # run Ansible playbook using our user
 ansible-playbook -i "$DOTFILES/hosts" "$DOTFILES/dotfiles.yml" --ask-become-pass
