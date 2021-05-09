44642067144f76684bcd245323af76e66c6a4324
jgero <33359660+jgero@users.noreply.github.com>
2021-05-09 15:21:01 +0200

    dconf gnome shortcuts

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # install ansible if it's missing
 dnf list installed "ansible" > /dev/null || sudo dnf install ansible
+# install community collection for dconf plugin
+ansible-galaxy collection install community.general
 
 # run Ansible playbook using our user
 ansible-playbook -i "$DOTFILES/hosts" "$DOTFILES/dotfiles.yml" --ask-become-pass
