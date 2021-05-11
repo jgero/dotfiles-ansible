6b324ec21ce90f5afaf604abff34fa41f8e7d5a1
jgero <33359660+jgero@users.noreply.github.com>
2021-05-12 00:00:31 +0200

    minor ssh and setup fixes

--- roles/bash/files/bashrc.d/aliases.bash
+++ roles/bash/files/bashrc.d/aliases.bash
@@ -1,5 +1,5 @@
 # ALIASES -------------------------------------------------------------------------------------
 
 # alias to nvim nightly
-alias vim="$HOME/.local/apps/squashfs-root/usr/bin/nvim"
+alias vim="nvim.appimage"
 
