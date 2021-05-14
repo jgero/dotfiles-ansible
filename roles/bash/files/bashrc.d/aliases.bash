2480be0a3cef3bae263bb51fa78abbc1fa3de877
jgero <33359660+jgero@users.noreply.github.com>
2021-05-14 21:57:59 +0200

    aliases for getting appointments from taskwarrior

--- roles/bash/files/bashrc.d/aliases.bash
+++ roles/bash/files/bashrc.d/aliases.bash
@@ -5,3 +5,8 @@ alias nvim="~/.local/bin/squashfs-root/usr/bin/nvim"
 # automatically use nvim instead of vim
 alias vim="nvim"
 
+# see my appointments
+alias appointment="appo"
+alias appointments="appo"
+alias appo="task +appointment list rc.dateformat:'Y-M-D H:N'"
+
